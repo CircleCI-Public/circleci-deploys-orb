d41c1aad912c9baf5a342cb3d6cca81b7fba62ff
Richie Chen <richie.chen@circleci.com>
2026-05-29 11:22:19 -0400

    shellcheck fix

--- src/scripts/plan.sh
+++ src/scripts/plan.sh
@@ -1,18 +1,15 @@
-COMPONENT_ARG=""
-ENV_ARG=""
-NAMESPACE_ARG=""
+#!/bin/bash
 VERSION="${PARAM_TARGET_VERSION:-${CIRCLE_SHA1:0:7}}"
+args=()
 if [ -n "$PARAM_COMPONENT_NAME" ]; then
-  COMPONENT_ARG="--component-name=$PARAM_COMPONENT_NAME"
+  args+=(--component-name="$PARAM_COMPONENT_NAME")
 fi
 if [ -n "$PARAM_ENVIRONMENT_NAME" ]; then
-  ENV_ARG="--environment-name=$PARAM_ENVIRONMENT_NAME"
+  args+=(--environment-name="$PARAM_ENVIRONMENT_NAME")
 fi
 if [ -n "$PARAM_NAMESPACE" ]; then
-  NAMESPACE_ARG="--namespace=$PARAM_NAMESPACE"
+  args+=(--namespace="$PARAM_NAMESPACE")
 fi
 circleci run release plan "$PARAM_DEPLOY_NAME" \
-  $COMPONENT_ARG \
-  $ENV_ARG \
-  $NAMESPACE_ARG \
+  "${args[@]}" \
   --target-version="$VERSION"
--- src/scripts/update_status.sh
+++ src/scripts/update_status.sh
@@ -1,7 +1,8 @@
-FAILURE_ARG=""
+#!/bin/bash
+args=()
 if [ -n "$PARAM_FAILURE_REASON" ]; then
-  FAILURE_ARG="--failure-reason=$PARAM_FAILURE_REASON"
+  args+=(--failure-reason="$PARAM_FAILURE_REASON")
 fi
 circleci run release update "$PARAM_DEPLOY_NAME" \
   --status="$PARAM_STATUS" \
-  $FAILURE_ARG
+  "${args[@]}"
